023b3f73e3022a4e483f341541dcddfcbcd3fdc2
ddorstijn <dannydorstijn1997@gmail.com>
2020-12-19 11:53:39 +0100

    Added task popup classes

--- src/Task.svelte
+++ src/Task.svelte
@@ -11,9 +11,16 @@
 	export let estimate;
 
 	let editing = false;
+  let showTooltip = false;
 
 	function startEdit() {
+		showTooltip = false;
 		editing = true; 
+	}
+
+	function removeTask() {
+		showTooltip = false;
+		dispatch('remove');
 	}
 
 	function resize(evt) {
@@ -32,92 +39,107 @@
   const popperOptions = {
 		placement: "left",
   };
-
-  let showTooltip = false;
 </script>
 
 <style>
-	ul {
-		display: flex;
-		align-items: center;
-	}
-
-	li {
+	.task-container {
 		display: flex;
 		justify-content: space-between;
 		padding: var(--space-2) var(--space-4);
 		border-radius: var(--rounded);
 	}
 
-	textarea {
-		background: transparent;
-		resize: none;
-	}
-
-	li > div {
+	.task-body {
 		width: 100%;
 		display: flex;
 		flex-direction: column;
 	}
 
-	li > div > div {
+	.task-details {
 		display: flex;
 		align-items: center;
 		gap: var(--space-2);
 	}
-
-	li > div > div > div {
-		display: flex;
-		align-items: center;
+	
+	.task-description {
+		font-size: var(--size-xl);
 	}
 
-	svg {
-		height: 1em;
+	.popup-tooltip {
+		display: flex;
+		align-items: center;
+
+		background: var(--tone-200);
+		color: var(--tone-800);
+		padding: var(--space-1);
+		border-radius: var(--rounded);
+		box-shadow: var(--shadow-lg);
+	}
+
+	.popup-tooltip > li {
+		padding: var(--space-2);
+
+		display: flex;
+		gap: var(--space-2);
+
+		border-radius: var(--rounded);
+	}
+
+	.popup-tooltip > li:hover {
+		cursor: pointer;
+		background: var(--tone-300);
+	}
+
+	.popup-edit:hover {
+		color: var(--blue);
+	}
+	
+	.popup-delete:hover {
+		color: var(--red);
+	}
+
+	[data-popper-arrow]::before {
+		background-color: var(--tone-200);
 	}
 </style>
 
-<li>
-	<div>
+<li class="task-container">
+	<div class="task-body">
 		{#if !editing}
-		<p>
+		<p class="task-description">
 			{ description }
 		</p>
 		{:else}
 		<textarea
 			bind:value="{description}"
 			rows="1"
+			class="task-description"
 			on:input="{resize}"
 			on:blur="{() => editing = false}"
 			on:keydown="{handleInput}"
 		/>
 		{/if}
-		<div>
-			<div>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-					<path
-						fill-rule="evenodd"
-						d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-				<p>
-					{ due.toLocaleString('en-gb', { month: 'short', day: 'numeric' }) }
-				</p>
-			</div>
+		<div class="task-details">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+				<path
+					fill-rule="evenodd"
+					d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+			<p>{ due.toLocaleString('en-gb', { month: 'short', day: 'numeric' }) }</p>
 
-			<div>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-					<path
-						fill-rule="evenodd"
-						d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-				<p>{ spent } / { estimate }</p>
-			</div>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+				<path
+					fill-rule="evenodd"
+					d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+			<p>{ spent } / { estimate }</p>
 		</div>
 	</div>
-	<button use:popperRef on:click="{() => showTooltip = !showTooltip}">
+	<button class="popup-reference" use:popperRef on:click="{() => showTooltip = !showTooltip}">
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			fill="none"
@@ -133,15 +155,15 @@
 		</svg>
 	</button>
 	{#if showTooltip}
-	<ul use:popperContent={popperOptions} use:clickOutside on:click_outside="{() => showTooltip = false}">
-		<li on:click="{startEdit}">
+	<ul class="popup-tooltip" use:popperContent={popperOptions} use:clickOutside on:click_outside="{() => showTooltip = false}">
+		<li class="popup-edit" on:click="{startEdit}">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
 				<path
 					d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
 				/>
 			</svg>
 		</li>
-		<li on:click="{() => dispatch('remove')}">
+		<li class="popup-delete" on:click="{removeTask}">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
 				<path
 					fill-rule="evenodd"
@@ -150,6 +172,7 @@
 				/>
 			</svg>
 		</li>
+		<div data-popper-arrow />
 	</ul>
 	{/if}
 </li>
