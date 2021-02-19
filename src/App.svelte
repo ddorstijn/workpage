d9fd1a7c029a3ab9ae724cca46feef65824321cd
ddorstijn <dannydorstijn1997@gmail.com>
2021-02-19 18:15:16 +0100

    Added theming possibility

--- src/App.svelte
+++ src/App.svelte
@@ -4,8 +4,57 @@
 	import Projects from './Projects.svelte';
 	import Tasks from './Tasks.svelte';
 	import Timer from './Timer.svelte';
+	import { onMount } from 'svelte';
+	import { themes } from './themes.js';
+
+	// Custom themes
+	let themeIndex = localStorage.getItem("themeIndex") || 0;
+
+	function nextTheme() {
+		themeIndex++;
+		if (themeIndex >= themes.length) {
+			themeIndex = 0;
+		}
+
+		console.log("Next Theme: " + themeIndex);
+		localStorage.setItem("themeIndex", themeIndex);
+		updateTheme();
+	}
+
+	function updateTheme() {
+		const root = document.documentElement;
+		const theme = themes[themeIndex];
+
+		root.style.setProperty('--red', theme.red);
+		root.style.setProperty('--orange', theme.orange);
+		root.style.setProperty('--yellow', theme.yellow);
+		root.style.setProperty('--green', theme.green);
+		root.style.setProperty('--aqua', theme.aqua);
+		root.style.setProperty('--blue', theme.blue);
+		root.style.setProperty('--purple', theme.purple);
+		root.style.setProperty('--gray-50', theme.gray["50"]);
+		root.style.setProperty('--gray-100', theme.gray["100"]);
+		root.style.setProperty('--gray-200', theme.gray["200"]);
+		root.style.setProperty('--gray-300', theme.gray["300"]);
+		root.style.setProperty('--gray-400', theme.gray["400"]);
+		root.style.setProperty('--gray-500', theme.gray["500"]);
+		root.style.setProperty('--gray-600', theme.gray["600"]);
+		root.style.setProperty('--gray-700', theme.gray["700"]);
+		root.style.setProperty('--gray-800', theme.gray["800"]);
+		root.style.setProperty('--gray-900', theme.gray["900"]);
+		root.style.setProperty('--gray-950', theme.gray["950"]);
+	}
+
+	onMount(() => {
+		updateTheme();
+	});
 </script>
 
+<button title="Switch theme" class="absolute text-gray-100 hover:text-gray-300" on:click="{nextTheme}">
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+		<path fill-rule="evenodd" d="M4 2a2 2 0 00-2 2v11a3 3 0 106 0V4a2 2 0 00-2-2H4zm1 14a1 1 0 100-2 1 1 0 000 2zm5-1.757l4.9-4.9a2 2 0 000-2.828L13.485 5.1a2 2 0 00-2.828 0L10 5.757v8.486zM16 18H9.071l6-6H16a2 2 0 012 2v2a2 2 0 01-2 2z" clip-rule="evenodd" />
+	</svg>
+</button>
 <main class="w-2/3 my-auto">
 	<Clock />
 	<Projects />
@@ -23,7 +72,7 @@
 
 	body {
 		@apply w-screen h-screen overflow-hidden p-16 flex gap-16 text-gray-900;
-		background: radial-gradient(#3c3836 0%, #282828 70%);
+		background: radial-gradient(var(--gray-100) 0%, var(--gray-50) 70%);
 	}
 
 	aside {
