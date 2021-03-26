39c5a3dda6bd4e8de698cfa4fbeee3ec25b19ec6
ddorstijn <dannydorstijn1997@gmail.com>
2021-03-26 15:44:12 +0100

    Cleanup before overwrite

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  semi: false,
-  singleQuote: true,
-  endOfLine: 'auto',
-  trailingComma: 'es5',
-  tabWidth: 2,
-}
--- src/vue-shim.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.vue' {
-  import { Component } from 'vue'
-  const _default: Component
-  export default _default
-}
-
-declare module 'vue-draggable-next' {
-  export { VueDraggableNext } from 'vue-draggable-next/dist/src/index'
-}
