fda2cb63b8f975190b1f20b6b3f4c5ea0d9f0191
ddorstijn <dannydorstijn1997@gmail.com>
2021-04-02 23:39:53 +0200

    Fixed build script

--- public/newtab/build/main.js
+++ public/newtab/build/main.js
@@ -1,4 +1,2 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
-export { V as default } from './main-a37b6cb8.js';
+export{M as default}from"./main-f4c36dc9.js";
 //# sourceMappingURL=main.js.map
