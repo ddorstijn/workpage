e0ee645aedc57a1ad3b7a964dfc07a77f212be44
ddorstijn <dannydorstijn@1997@gmail.com>
2023-06-15 18:52:03 +0200

    Latest commit

--- src/pages/newtab/main.ts
+++ src/pages/newtab/main.ts
@@ -1,8 +1,7 @@
 import App from "./App.svelte";
 import "@/lib/i18n";
+import '../../app.postcss';
 
-const app = new App({
-  target: document.body,
-});
+const app = new App();
 
 export default app;
