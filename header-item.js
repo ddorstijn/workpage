6468dda3560ccd556dd6d4a9410d6fca3caf57f8
ddorstijn <dannydorstijn@1997@gmail.com>
2023-11-24 22:50:31 +0100

    First webcomponent

---
+++ header-item.js
@@ -0,0 +1,26 @@
+customElements.define(
+    "wp-dialog",
+    class extends HTMLElement {
+        /** @type {HTMLDialogElement} */
+        #dialog;
+
+        constructor() {
+            super().attachShadow({ mode: 'open' }).append(document.getElementById('wp-dialog').content.cloneNode(true));
+        }
+
+        connectedCallback() {
+            this.#dialog = this.shadowRoot.querySelector('dialog');
+            this.#dialog.addEventListener('click', event => {
+                let rect = this.#dialog.getBoundingClientRect();
+                if (event.clientY < rect.top || event.clientY > rect.bottom) return this.#dialog.close();
+                if (event.clientX < rect.left || event.clientX > rect.right) return this.#dialog.close();
+            });
+
+            this.shadowRoot.querySelector('header h2').innerText = this.title;
+        }
+
+        showModal() {
+            this.#dialog.showModal();
+        }
+    },
+);
