b75ee8c350fb2b414509b951ccf9978264ffcdcc
ddorstijn <dannydorstijn1997@gmail.com>
2023-11-25 16:45:30 +0100

    Added links component

---
+++ wp-links.js
@@ -0,0 +1,46 @@
+customElements.define(
+  "wp-links",
+  class extends HTMLElement {
+    constructor() {
+      super()
+        .attachShadow({ mode: "open" })
+        .append(document.getElementById("wp-links").content.cloneNode(true));
+    }
+  }
+);
+
+customElements.define(
+  "wp-link-group",
+  class extends HTMLElement {   
+    constructor() {
+      super()
+        .attachShadow({ mode: "open" })
+        .append(document.getElementById("wp-link-group").content.cloneNode(true));
+    }
+
+    connectedCallback() {
+      this.shadowRoot.querySelector("h2").innerText =
+        this.dataset['title'];
+
+        this.shadowRoot.querySelector("section").classList.add(this.dataset['color']);
+    }
+  }
+);
+
+customElements.define(
+  "wp-link-group-item",
+  class extends HTMLElement {
+    constructor() {
+      super()
+        .attachShadow({ mode: "open" })
+        .append(document.getElementById("wp-link-group-item").content.cloneNode(true));
+    }
+
+    connectedCallback() {
+      let anchor = this.shadowRoot.querySelector("a");
+      anchor.innerText = this.dataset['title'];
+      anchor.href = this.dataset['url'];
+    }
+  }
+);
+  
