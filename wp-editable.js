e9e516ce718def6444ae507d19f4e7a85a703e74
ddorstijn <dannydorstijn@1997@gmail.com>
2023-11-26 22:43:33 +0100

    Started work on editable part of editable

--- wp-editable.js
+++ wp-editable.js
@@ -4,5 +4,20 @@ customElements.define(
         constructor() {
             super().attachShadow({ mode: 'open' }).append(document.getElementById(this.nodeName).content.cloneNode(true));
         }
+
+        connectedCallback() {
+            this.shadowRoot.querySelector('.edit').addEventListener('click', () => this.edit());
+            this.shadowRoot.querySelector('.delete').addEventListener('click', () => this.delete());
+        }
+
+        edit() {
+            /** @type {HTMLSlotElement} */
+            this.shadowRoot.querySelector('slot').hidden = true;
+            this.shadowRoot.querySelector('input').hidden = false;
+        }
+
+        delete() {
+            this.dispatchEvent(new Event('delete'));
+        }
     },
 );
