2a3709a3472ce1f5435a2df717a9f377a01dd9f1
ddorstijn <dannydorstijn@1997@gmail.com>
2023-11-26 23:21:54 +0100

    Editable tasks

--- wp-editable.js
+++ wp-editable.js
@@ -1,24 +1,46 @@
 customElements.define(
   "wp-editable",
   class extends HTMLElement {
+    /** @type {HTMLInputElement} */
+    #input;
+
+    /** @type {HTMLElement} */
+    #title;
+    
     constructor() {
       /** @type {HTMLElement} */
       let node = document.getElementById(super().nodeName).content.cloneNode(true);
-      node.querySelector('.edit').addEventListener('click', () => this.edit());
       node.querySelector('.delete').addEventListener('click', () => this.delete());
-
+      node.querySelector('.edit').addEventListener('click', () => this.edit());
+      
+      this.#input = node.querySelector('input');
+      this.#input.addEventListener('blur', (ev) => this.blur(ev));
       this.attachShadow({ mode: 'open' }).append(node);
     }
+    
+    connectedCallback() {
+      this.#title = this.shadowRoot.querySelector('slot').assignedElements()[0];
 
-
+      this.#input.value = this.#title.innerText;
+    }
+    
     edit() {
-      /** @type {HTMLSlotElement} */
       this.shadowRoot.querySelector('slot').hidden = true;
-      this.shadowRoot.querySelector('input').hidden = false;
+      this.shadowRoot.querySelector('.actions').hidden = true;
+
+      this.#input.hidden = false;
+      this.#input.focus();      
     }
 
     delete() {
       this.dispatchEvent(new Event('delete'));
+    }
+
+    blur(ev) {
+      this.#title.innerText = this.#input.value;
+
+      this.shadowRoot.querySelector('slot').hidden = false;
+      this.shadowRoot.querySelector('.actions').hidden = false;
     }
   },
 );
