436c326185ff4e90b36c5d777f64a48147ccfc6a
ddorstijn <dannydorstijn1997@gmail.com>
2023-11-29 12:30:52 +0100

    Work on sortable action

--- components/wp-links.js
+++ components/wp-links.js
@@ -4,6 +4,22 @@ customElements.define(
     constructor() {
       /** @type {HTMLElement} */
       let node = document.getElementById(super().nodeName).content.cloneNode(true);
+      const links = node.querySelector('div');
+      for (const group of window.project.linkgroups) {
+        const groupEl = document.createElement('wp-link-group');
+        groupEl.dataset['name'] = group.name;
+        groupEl.dataset['color'] = group.color;
+
+        for (const link of group.links) {
+          const linkEl = document.createElement('wp-link');
+          linkEl.dataset['name'] = link.name;
+          linkEl.dataset['url'] = link.url;
+          console.log(linkEl);
+          groupEl.querySelector('wp-sortable').append(linkEl);
+        }
+
+        links.append(groupEl);
+      }
       
       this.attachShadow({ mode: "open" }).append(node);
     }
@@ -16,7 +32,7 @@ customElements.define(
     constructor() {
       /** @type {HTMLElement} */
       let node = document.getElementById(super().nodeName).content.cloneNode(true);
-      node.querySelector('h2').innerText = this.dataset['title'];
+      node.querySelector('h2').innerText = this.dataset['name'];
       node.querySelector("section").classList.add(this.dataset['color']);
       
       this.attachShadow({ mode: "open" }).append(node);
@@ -31,7 +47,7 @@ customElements.define(
       /** @type {HTMLElement} */
       let node = document.getElementById(super().nodeName).content.cloneNode(true);
       let a = node.querySelector("a");
-      a.innerText = this.dataset['title'];
+      a.innerText = this.dataset['name'];
       a.href = this.dataset['url'];
 
       this.attachShadow({ mode: "open" }).append(node);
---
+++ js/sortable.js
@@ -0,0 +1,104 @@
+[...document.querySelectorAll('ol[sortable]')].forEach(el => {
+    /** @type {HTMLElement} */
+    let draggingEl;
+    /** @type {HTMLElement} */
+    let placeholder;
+    /**@type {boolean} */
+    let isDraggingStarted = false;
+
+    let x = 0;
+    let y = 0;
+
+    /** @param ev {MouseEvent} */
+    const mouseDownHandler = ev => {
+      draggingEl = ev.target.getRootNode().host;
+
+      // Calculate the mouse position
+      const rect = draggingEl.getBoundingClientRect();
+      x = ev.pageX - rect.left;
+      y = ev.pageY - rect.top;
+
+      // Attach the listeners to document
+      document.addEventListener('mousemove', mouseMoveHandler);
+      document.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    const mouseMoveHandler = (e) => {
+      const draggingRect = draggingEl.getBoundingClientRect();
+
+      if (!isDraggingStarted) {
+        isDraggingStarted = true;
+
+        // Let the placeholder take the height of dragging element
+        // So the next element won't move up
+        placeholder = document.createElement('wp-placeholder');
+        draggingEl.parentNode.insertBefore(placeholder, draggingEl.nextSibling);
+        placeholder.style.height = draggingRect.height + 'px';
+      }
+
+      // Set position for dragging element
+      draggingEl.style.position = 'absolute';
+      draggingEl.style.top = (e.pageY - y) + 'px';
+      draggingEl.style.left = (e.pageX - x) + 'px';
+
+      const prevEl = draggingEl.previousElementSibling;
+      const nextEl = placeholder.nextElementSibling;
+
+      if (prevEl && this.isAbove(draggingEl, prevEl)) {
+        this.swap(placeholder, draggingEl);
+        this.swap(placeholder, prevEl);
+        return;
+      }
+
+      if (nextEl && this.isAbove(nextEl, draggingEl)) {
+        this.swap(nextEl, placeholder);
+        this.swap(nextEl, draggingEl);
+      }
+    };
+
+    const mouseUpHandler = () => {
+      placeholder?.remove();
+
+      draggingEl.style.removeProperty('top');
+      draggingEl.style.removeProperty('left');
+      draggingEl.style.removeProperty('position');
+
+      x = null;
+      y = null;
+      draggingEl = null;
+      isDraggingStarted = false;
+
+      // Remove the handlers of mousemove and mouseup
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    /** 
+     *  @param {HTMLElement} nodeA 
+     *  @param {HTMLElement} nodeB  
+     */
+    function swap(nodeA, nodeB) {
+        const parentA = nodeA.parentNode;
+        const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
+
+        nodeB.parentNode.insertBefore(nodeA, nodeB);
+        parentA.insertBefore(nodeB, siblingA);
+    }
+
+    /** 
+     *  @param {HTMLElement} nodeA 
+     *  @param {HTMLElement} nodeB  
+     */
+    function isAbove (nodeA, nodeB) {
+        // Get the bounding rectangle of nodes
+        const rectA = nodeA.getBoundingClientRect();
+        const rectB = nodeB.getBoundingClientRect();
+
+        return rectA.top + rectA.height / 2 < rectB.top + rectB.height / 2;
+    };
+
+    // Query all items
+    [...this.querySelectorAll('.draggable')].forEach(item => {
+      item.shadowRoot.querySelector('.drag-handle').addEventListener('mousedown', mouseDownHandler);
+    });
+});
