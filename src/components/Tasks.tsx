fb77047a64a50df17a7b0464b5b54d28a5701f6e
ddorstijn <dannydorstijn1997@gmail.com>
2023-12-13 09:05:41 +0100

    Added proper tasks

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,9 +1,42 @@
-import { Component } from "solid-js";
+import { Component, For } from "solid-js";
+import AddIcon from "~icons/material-symbols/add";
+import ChevronLeftIcon from "~icons/material-symbols/chevron-left-rounded";
 
-const Tasks: Component = () => {
+import styles from "./Tasks.module.css";
+
+const Tasks: Component<{ tasks: Task[] }> = (props) => {
   return (
-    <></>
+    <details class={styles["task-drawer"]}>
+      <summary>
+        <span>Tasks</span>
+        <ChevronLeftIcon />
+      </summary>
+      <section class={styles["tasks"]}>
+        <header>
+          <h3 class="active">Todo</h3>
+          <h3>Done</h3>
+        </header>
+        <ol>
+          <For each={props.tasks}>
+            { (item) => <Task task={item} /> }
+          </For>
+        </ol>
+        <button class={styles["add-task"]}>
+          <AddIcon />
+          <span>Add a task</span>
+        </button>
+      </section>
+    </details>
   )
 }
 
 export default Tasks;
+
+const Task: Component<{task: Task}> = (props) => {
+  return (
+    <li class={styles["task-list_item"]}>
+      <div class={styles["handle"]}></div>
+      <span>{props.task.name}</span>
+    </li>
+  )
+}
--- src/workpage.d.ts
+++ src/workpage.d.ts
@@ -2,6 +2,6 @@ type Link = { name: string, url: URL };
 
 type LinkGroup = { name: string, color: string, links: Link[] };
 
-type Task = { name: String };
+type Task = { name: string };
 
 type Project = { used: Date, todo: Task[], done: Task[], linkgroups: Linkgroup[] };
