d7d9009eb586d33ae76d99d8ecd6c613df13cf90
ddorstijn <dannydorstijn@1997@gmail.com>
2024-01-10 15:28:59 +0100

    FIX Chrome version with cmd

--- run.cmd
+++ run.cmd
@@ -1,1 +1,8 @@
-web-ext run --start-url "about:debugging#/runtime/this-firefox"  -f deved
+@ECHO OFF
+
+SET program=%1
+IF NOT DEFINED program (
+    web-ext run --start-url "about:debugging#/runtime/this-firefox" -f deved
+) ELSE (
+    web-ext run -t chromium
+)
