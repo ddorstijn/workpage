79f43ac004072ed542e584bfddac0774b8db86fd
ddorstijn <dannydorstijn@1997@gmail.com>
2024-08-07 08:56:11 +0200

    Use dialog to add projects, groups and links

--- src/newtab/Workpage.tsx
+++ src/newtab/Workpage.tsx
@@ -31,17 +31,12 @@ export const Workpage: Component = () => {
         await refetchCurrent();
     }
 
-    async function deleteProject(project: Bookmarks.BookmarkTreeNode) {
-        await recursiveDeleteBookmarks(project);
-        await refetchProjects();
+    async function addProject(e: SubmitEvent) {
+        e.preventDefault();
 
-        if (currentProject()?.id === project.id) {
-            await setCurrentProject(null);
-        }
-    }
-
-    async function addProject() {
-        const title = prompt("What is the project name?")
+        const form = e.target as HTMLFormElement;
+        const formData = new FormData(form);
+        const title = formData.get("title") as string;
 
         if (!title) {
             return;
@@ -53,20 +48,39 @@ export const Workpage: Component = () => {
 
         await bookmarks.create({ title, parentId: root()!.id, index });
         await refetchProjects();
+
+        form.reset();
+        form.closest('dialog')?.close();
     }
 
-    async function addGroup() {
+    async function deleteProject(project: Bookmarks.BookmarkTreeNode) {
+        await recursiveDeleteBookmarks(project);
+        await refetchProjects();
+
+        if (currentProject()?.id === project.id) {
+            await setCurrentProject(null);
+        }
+    }
+
+    async function addGroup(e: SubmitEvent) {
+        e.preventDefault();
+
         if (!currentProject()) {
             return;
         }
 
-        const title = prompt("What is the group name?");
+        const form = e.target as HTMLFormElement;
+        const formData = new FormData(form);
+        const title = formData.get("title") as string;
         if (!title) {
             return;
         }
 
         await bookmarks.create({ title, parentId: currentProject()!.id });
         await refetchCurrent();
+
+        form.reset();
+        form.closest('dialog')?.close();
     }
 
     async function deleteGroup(group: Bookmarks.BookmarkTreeNode) {
@@ -74,9 +88,28 @@ export const Workpage: Component = () => {
         await refetchCurrent();
     }
 
-    async function addLink(groupId: string) {
-        await bookmarks.create({ title: "Link", url: "https://google.com", parentId: groupId });
+    async function addLink(e: SubmitEvent) {
+        e.preventDefault();
+
+        if (!currentProject()) {
+            return;
+        }
+
+        const form = e.target as HTMLFormElement;
+        const formData = new FormData(form);
+        const title = formData.get("title") as string;
+        const url = formData.get("url") as string;
+        const parentId = formData.get("group") as string;
+
+        if (!title || !url || !parentId) {
+            return;
+        }
+
+        await bookmarks.create({ title, url, parentId });
         await refetchCurrent();
+
+        form.reset();
+        form.closest('dialog')?.close();
     }
 
     async function deleteLink(link: Bookmarks.BookmarkTreeNode) {
@@ -87,8 +120,53 @@ export const Workpage: Component = () => {
     return (
         <div>
             <h1>Workpage</h1>
-            <button onClick={addProject}>Add Project</button>
-            <button onClick={addGroup}>Add group</button>
+            <button onClick={() => document.getElementById("add-project")?.showModal()}>Add Project</button>
+            <dialog id="add-project">
+                <form method="dialog" onSubmit={addProject}>
+                    <label>
+                        Title
+                        <input name="title" type="text" />
+                    </label>
+                    <button type="submit">Add</button>
+                </form>
+            </dialog>
+
+            <button onClick={() => document.getElementById("add-group")?.showModal()}>Add group</button>
+
+            <dialog id="add-group">
+                <form onSubmit={addGroup}>
+                    <label>
+                        Title
+                        <input name="title" type="text" />
+                    </label>
+                    <button type="submit">Add</button>
+                </form>
+            </dialog>
+
+            <button onClick={() => document.getElementById("add-link")?.showModal()}>Add link</button>
+
+            <dialog id="add-link">
+                <form onSubmit={addLink}>
+                    <label>
+                        Title
+                        <input name="title" type="text" />
+                    </label>
+                    <label>
+                        URL
+                        <input name="url" type="text" />
+                    </label>
+                    <label>
+                        Group
+                        <select name="group">
+                            <For each={currentProject()?.children}>
+                                {(group) => <option value={group.id}>{group.title}</option>}
+                            </For>
+                        </select>
+                    </label>
+                    <button type="submit">Add</button>
+                </form>
+            </dialog>
+
             <span>{currentProject() ? `Current project: ${currentProject()!.title}` : ""}</span>
             <h2>Projects</h2>
             <ol>
@@ -110,7 +188,6 @@ export const Workpage: Component = () => {
                     {(group) =>
                         <li>
                             <h3>{group.title}</h3>
-                            <button onClick={() => addLink(group.id)}>Add link</button>
                             <button onClick={() => deleteGroup(group)}>Delete</button>
                             <ol>
                                 <For each={group.children}>
